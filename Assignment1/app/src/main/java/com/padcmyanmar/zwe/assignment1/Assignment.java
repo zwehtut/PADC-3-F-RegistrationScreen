0bbef3560b55d9a27eedc9d8e00572646ded3f21
hp\ZWE <mtkmzh@gmail.com>
2017-11-25 13:25:58 +0630

    Step 1 for registration assignment

---
+++ Assignment1/app/src/main/java/com/padcmyanmar/zwe/assignment1/Assignment.java
@@ -0,0 +1,17 @@
+package com.padcmyanmar.zwe.assignment1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZWE on 10/11/2017.
+ */
+
+public class Assignment extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.assignment_main);
+    }
+}
